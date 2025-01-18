e88b8d7905dad70b678e061ee92575638bcf9606
Shubhansh <shubhanshvaish12@gmail.com>
2025-01-18 16:58:04 +0530

    updatesss

--- Frontend/src/assets/api.js
+++ Frontend/src/assets/api.js
@@ -4,7 +4,8 @@ const api=axios.create({
     baseURL: 'https://hospital-management-3tyt.onrender.com/auth',
 });
 
-const googleAuth = (code) => api.get(`/google?code=${code}`);
-
+const googleAuth = (code) => {
+    return api.get(`/google?code=${code}&redirect_uri=https://hospital-management-3tyt.onrender.com/auth/google`);
+  };
 
 export default googleAuth;
