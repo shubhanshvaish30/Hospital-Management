a645f343acf1d31a55b8bfac1b0adf1aafa4129f
Shubhansh <shubhanshvaish12@gmail.com>
2025-01-18 16:51:07 +0530

    backend updates

--- Backend/utils/googleConfig.js
+++ Backend/utils/googleConfig.js
@@ -6,7 +6,7 @@ const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET;
 const oauth2client= new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    'postmessage'
+    'https://hospital-management-3tyt.onrender.com/auth/google'
 )
 
 export default oauth2client
