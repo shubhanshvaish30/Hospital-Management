8ceff240617803f77f5f43f13fe2cda51f802a89
Shubhansh Vaish <122173772+shubhanshvaish30@users.noreply.github.com>
2025-01-22 18:56:20 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { googleLogin, sendClientKey } from "../controllers/authController.js";
+import { googleLogin, login, sendClientKey, signup } from "../controllers/authController.js";
 
 const authRouter=express.Router();
 
 
-authRouter.get('/google',googleLogin)
+authRouter.get('/google', googleLogin)
 authRouter.get('/getClientId',sendClientKey);
+authRouter.post('/signup',signup);
+authRouter.post('/login',login);
 
 
 export default authRouter;
